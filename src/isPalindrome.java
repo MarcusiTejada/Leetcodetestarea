f422ee10d840b3f5536048ff6ec671842db47356
MarcusiTejada <martejada11@gmail.com>
2024-10-21 18:51:49 -0400

    //Time: O(N) //Space: O(N)
    Signed-off-by: MarcusiTejada <martejada11@gmail.com>

---
+++ src/isPalindrome.java
@@ -0,0 +1,12 @@
+public class isPalindrome {
+    class Solution {
+        public boolean isPalindrome(int x) {
+            boolean answer;
+            String basic = Integer.toString(x);
+            StringBuilder sb = new StringBuilder(basic);
+            String flip = sb.reverse().toString();
+            answer = basic.equals(flip);
+            return answer;
+        }
+    }
+}
