e60513d4cc4530d2fe480cb0ad7badf05a7f0db2
MarcusiTejada <martejada11@gmail.com>
2024-10-21 18:12:22 -0400

    //Time: O(N) //Space: O(N)
    Signed-off-by: MarcusiTejada <martejada11@gmail.com>

---
+++ src/TwoSumJavaSolution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class TwoSumJavaSolution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        Map <Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int cur = nums[i];
+            int y = target - cur;
+            if(map.containsKey(y)){
+                answer[0] = i;
+                answer[1] = map.get(y);
+            }else{
+                map.put(cur,i);
+            }
+        }
+        return answer;
+    }
+}
