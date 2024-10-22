6ea8c30f749758f6600003cf784ebc07d6dda2bb
MarcusiTejada <martejada11@gmail.com>
2024-10-22 12:00:35 -0400

    Cut run time by a single ms and space by 0.1 mb took out the arraylist no need to hold onto the numbers just add them could still be faster, but don't know how yet
    Signed-off-by: MarcusiTejada <martejada11@gmail.com>

--- src/RomanNumeralCalc.java
+++ src/RomanNumeralCalc.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,54 +12,50 @@ public class RomanNumeralCalc {
         key.put('C', 100);
         key.put('D', 500);
         key.put('M', 1000);
-        ArrayList<Integer> numbers = new ArrayList<>();
         char[] roman = s.toCharArray();
         int i = 0;
 
         while (i < roman.length) {
             if (i + 1 == roman.length) {
-                numbers.add(key.get(roman[i]));
+                answer = answer + key.get(roman[i]);
                 i++;
             } else {
                 if (roman[i] == (roman[i + 1])) {
-                    numbers.add(key.get(roman[i]));
+                    answer = answer + key.get(roman[i]);
                     i++;
                 } else {
                     String str = "" + roman[i] + roman[i + 1];
                     switch (str) {
                         case "IV":
-                            numbers.add(4);
+                            answer = answer + 4;
                             i = i + 2;
                             break;
                         case "IX":
-                            numbers.add(9);
+                            answer = answer + 9;
                             i = i + 2;
                             break;
                         case "XL":
-                            numbers.add(40);
+                            answer = answer + 40;
                             i = i + 2;
                             break;
                         case "XC":
-                            numbers.add(90);
+                            answer = answer + 90;
                             i = i + 2;
                             break;
                         case "CD":
-                            numbers.add(400);
+                            answer = answer + 400;
                             i = i + 2;
                             break;
                         case "CM":
-                            numbers.add(900);
+                            answer = answer + 900;
                             i = i + 2;
                             break;
                         default:
-                            numbers.add(key.get(roman[i]));
+                            answer = answer + key.get(roman[i]);
                             i++;
                     }
                 }
             }
-        }
-        for (int number : numbers) {
-            answer += number;
         }
 
         return answer;
