f301bee7eca84024cd7e6622b003377731f2c7cb
MarcusiTejada <martejada11@gmail.com>
2024-11-18 19:54:13 -0500

    Time Complexity: O(n) Space Complexity: O(n) (due to the stack in the worst case).
    * n being the length of the string
    
    Signed-off-by: MarcusiTejada <martejada11@gmail.com>

---
+++ src/ValidParentheses.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        boolean answer = false;
+
+        if (s.charAt(0) == '}' || s.charAt(0) == ']' || s.charAt(0) == ')' || s.length() == 1) {
+            i = s.length();
+        }
+
+        for (; i < s.length(); i++) {
+            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+                answer = true;
+            } else if (s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+                answer = true;
+            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+                answer = true;
+            } else if (s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+                answer = true;
+            } else {
+                answer = false;
+                i = s.length();
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
+
