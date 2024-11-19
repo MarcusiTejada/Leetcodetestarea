abc5daca5d922e6dadd9e38e56e245cf6c85947e
MarcusiTejada <martejada11@gmail.com>
2024-11-19 13:43:07 -0500

    Took time to understand that we are always looking at a node and never at the entire list itself and how an instantiated node even if empty/null is given a default value of 0.
    Time Complexity: O(m + n)
    Space Complexity: O(m + n)
    Signed-off-by: MarcusiTejada <martejada11@gmail.com>

---
+++ src/merge/MergeTwoSortedLists.java
@@ -0,0 +1,28 @@
+package merge;
+
+public class MergeTwoSortedLists {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode answer = null;
+
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1 == null && list2 == null) {
+            return answer;
+        }
+
+        if (list1.val <= list2.val) {
+            answer = list1;
+            answer.next = mergeTwoLists(list1.next, list2);
+        } else {
+            answer = list2;
+            answer.next = mergeTwoLists(list1, list2.next);
+        }
+
+
+        return answer;
+    }
+}
+
+
